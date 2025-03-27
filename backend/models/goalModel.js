de82c4745de9db5668e78c3682d487bec20b1a5d
bigbawz <alvinfulbert.business@gmail.com>
2025-03-27 13:12:22 +0700

    Added userModel.js

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -3,6 +3,9 @@
 import mongoose from "mongoose";
 
 const goalSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId
+    },
     text: {
         type: String,
         required: [true, 'Please add a text value']
